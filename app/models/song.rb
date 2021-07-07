97be86d2341fc8149b4f2c65174792b893bea5bc
Bill <billfeng2002@gmail.com>
2021-07-07 11:46:31 -0700

    Done.

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,3 +1,17 @@
 class Song < ActiveRecord::Base
   # add associations here
+  belongs_to :artist
+  belongs_to :genre
+  has_many :notes
+
+  def artist_name
+    if(self.artist)
+      self.artist.name
+    end
+  end
+
+  def artist_name= (name)
+    self.artist=Artist.find_or_create_by(name: name)
+    self.save
+  end
 end
